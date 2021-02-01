eda53944ad08acbf6eea7477ebfac3fddd7e0e8d
sch0lars <tph1994@gmail.com>
2021-01-31 18:12:26 -0700

    Finished the backpropagation code.

--- neural_net_calc.py
+++ neural_net_calc.py
@@ -1,11 +1,13 @@
+from copy import copy
 from math import e
 
 x = [1, 1, 1]
 w1 = [[0.4, 0.2, 0.9], [0.3, 0.1, 0.4]]
 w2 = [[0.8, 0.7], [-0.6, -0.4]]
 target_outputs = [1, 0]
+eta = 0.9
 
-def calculate(x, w1, w2, target_outputs):
+def calculate(x, w1, w2, target_outputs, eta):
     print('Calculate h')
     print('-'*20)
     hidden_nodes = []
@@ -18,7 +20,7 @@ def calculate(x, w1, w2, target_outputs):
         equation_string = equation_string[:-3] + f' = {s:.3f}'
         print(equation_string)
         h = 1/(1+pow(e, -s))
-        print(f'h{i+1} = 1/(1+e^-{s:.3f}) = {h:.3f}\n')
+        print(f'h_{i+1} = 1/(1+e^-{s:.3f}) = {h:.3f}\n')
         hidden_nodes.append(h)
 
 
@@ -34,7 +36,7 @@ def calculate(x, w1, w2, target_outputs):
         equation_string = equation_string[:-3] + f' = {s:.3f}'
         print(equation_string)
         o = 1/(1+pow(e, -s))
-        print(f'o{i+1} = 1/(1+e^{-1*s:.3f}) = {o:.3f}\n')
+        print(f'o_{i+1} = 1/(1+e^{-1*s:.3f}) = {o:.3f}\n')
         output_nodes.append(o)
         
 
@@ -43,7 +45,7 @@ def calculate(x, w1, w2, target_outputs):
     output_node_errors = []
     for i, node in enumerate(output_nodes):
         node_error = node * (1 - node) * (target_outputs[i] - node)
-        print(f'd{i+1}: {node:.3f} * (1 - {node:.3f}) * ({target_outputs[i]} - {node:.3f}) = {node_error:.3f}')
+        print(f'd_{i+1}: {node:.3f} * (1 - {node:.3f}) * ({target_outputs[i]} - {node:.3f}) = {node_error:.3f}')
         output_node_errors.append(node_error)
 
 
@@ -59,8 +61,48 @@ def calculate(x, w1, w2, target_outputs):
         equation_string = equation_string[:-3] + f' = {s:.3f}'
         hi = hidden_nodes[i] * (1 - hidden_nodes[i]) * s
         print(equation_string)
-        print(f'h{i+1}: {hidden_nodes[i]:.3f} * (1 - {hidden_nodes[i]:.3f}) * {s:.3f} = {hi:.3f}\n')
-            
+        print(f'h_{i+1}: {hidden_nodes[i]:.3f} * (1 - {hidden_nodes[i]:.3f}) * {s:.3f} = {hi:.3f}\n')
+        hidden_node_errors.append(hi)
 
 
-calculate(x, w1, w2, target_outputs)
+    print('\nCalculate weight changes')
+    print('-'*20)  
+    w2_weight_changes = []
+    w1_weight_changes = []
+    for i, h in enumerate(hidden_nodes):
+        for j, one in enumerate(output_node_errors):
+            weight_change = eta * one * h
+            print(f'W2_{i+1}{j+1}: {eta:.3f} * {one:.3f} * {h:.3f} = {weight_change:.4f}')
+            w2_weight_changes.append(weight_change)
+    print()
+    for i, xi in enumerate(x):
+        for j, hne in enumerate(hidden_node_errors):
+            weight_change = eta * hne * xi
+            print(f'W1_{i+1}{j+1}: {eta:.3f} * {hne:.3f} * {xi:.3f} = {weight_change:.4f}')
+            w1_weight_changes.append(weight_change)
+
+    
+    print('\nUpdate weights')
+    print('-'*20)
+    new_w2 = []
+    new_w1 = []
+    w2_wc_values = copy(w2_weight_changes)
+    w1_wc_values = copy(w1_weight_changes)
+    for i in range(len(w2[0])):
+        for j in range(len(w2)):
+            weight_change = w2_wc_values.pop(0)
+            new_weight = w2[j][i] + weight_change
+            sign = '-' if weight_change < 0 else '+'
+            print(f'New W2_{i+1}{j+1}: {w2[j][i]:.3f} {sign} {weight_change:.3f} = {new_weight:.3f}')
+            new_w2.append(new_weight)
+    print()
+    for i in range(len(w1[0])):
+        for j in range(len(w1)):
+            weight_change = w1_wc_values.pop(0)
+            new_weight = w1[j][i] + weight_change
+            sign = '-' if weight_change < 0 else '+'
+            print(f'New W1_{i+1}{j+1}: {w1[j][i]:.3f} {sign} {weight_change:.3f} = {new_weight:.3f}')
+            new_w1.append(new_weight)
+
+    
+# calculate(x, w1, w2, target_outputs, 0.9)
